8b2b230454a1df9cf527987fa403286044365c93
SolomonZinabu <solomonzinabu56@gmail.com>
2024-06-25 16:53:10 +0300

    skills updated

--- components/Skills.js
+++ components/Skills.js
@@ -18,42 +18,63 @@ function Skills() {
             Frontend
           </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">JavaScript</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
+              <Typography variant="body1">TypeScript</Typography>
+            </Grid>
+            <Grid item xs={3}>
               <Typography variant="body1">React</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
+              <Typography variant="body1">Next.js</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body1">React Native</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body1">Angular</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body1">Ionic</Typography>
+            </Grid>
+            <Grid item xs={3}>
               <Typography variant="body1">HTML</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">CSS</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Material-UI</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body1">Tailwind CSS</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body1">Bootstrap</Typography>
             </Grid>
           </Grid>
           <Typography variant="h6" component="h3" gutterBottom style={{ marginTop: '16px' }}>
             Backend
           </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Node.js</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Express.js</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Python</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Django</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">RESTful APIs</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">GraphQL</Typography>
             </Grid>
           </Grid>
@@ -61,16 +82,16 @@ function Skills() {
             Database
           </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">MySQL</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">PostgreSQL</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">MongoDB</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">SQLite</Typography>
             </Grid>
           </Grid>
@@ -78,22 +99,22 @@ function Skills() {
             DevOps and Server Management
           </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">AWS</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Docker</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Nginx</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">CI/CD</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Production Deployment</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Server Debugging</Typography>
             </Grid>
           </Grid>
@@ -101,10 +122,10 @@ function Skills() {
             Other Skills
           </Typography>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Git</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Typography variant="body1">Agile Methodologies</Typography>
             </Grid>
           </Grid>
