6417bc44af90d5b6fb08cdf3db8b0905536625c6
SolomonZinabu <solomonzinabu56@gmail.com>
2024-06-25 16:07:06 +0300

    project description added for AWS

--- components/Projects.js
+++ components/Projects.js
@@ -16,22 +16,22 @@ const projects = [
   },
   {
     title: "Amazon Web Services API Integration",
-    description: `I have done more than 20 API integrations to our vendors/clients through AWS Lambda and EC2.`,
+    description: `I spearheaded the integration of over 20 APIs for our vendors and clients utilizing AWS Lambda and EC2. This project involved designing, developing, and deploying serverless functions and virtual server instances to facilitate seamless and scalable API interactions. Leveraging AWS's robust infrastructure, I ensured high availability, low latency, and secure data transmission. The integrations covered a wide range of functionalities, including data synchronization, payment processing, user authentication, and third-party service communications. This initiative significantly improved operational efficiency and enabled our clients to leverage advanced cloud capabilities for their business needs.`,
     image: '/imgs/amazon.png'
   },
   {
     title: "Application Level Traffic Router",
-    description: `I have built a traffic router to a Fettan core banking system which has API integration to secured vendors.`,
+    description: `I architected and developed two sophisticated traffic router applications for the Fettan core banking system, enhancing security and efficiency in API traffic management. The first router, the DMZ router, is designed to route traffic securely to AWS, effectively preventing direct API calls to the database server. The second router, the Secure Router, manages traffic to other clients, such as government organizations, that require direct API calls from our system. This dual-layer routing strategy significantly bolsters our system's security and operational integrity, ensuring robust and reliable communication channels.`,
     image: '/imgs/router.png'
   },
   {
     title: "mPOS",
-    description: `I have led the development team and was the lead developer for mPOS applications development which has both Android and web support with one code base. This application has a lot of features like two-level authorization, purchase, pay bills, supply order (merchant to merchant transaction), cash in, cash out, merchant registration, customer registration.`,
+    description: `As the lead developer, I directed the development team for the mPOS application, ensuring seamless functionality across both Android and web platforms from a single code base. This robust application is designed to meet diverse merchant needs, featuring two-level authorization, purchase and payment capabilities, bill payments, and supply orders (merchant to merchant transactions). It also supports cash in/out operations, comprehensive merchant and customer registration processes, receipt printing, and End-of-Day (EOD) reports. The mPOS system is engineered to streamline merchant operations, enhancing efficiency and user experience with its wide array of advanced functionalities.`,
     image: '/imgs/mposwebpurchasebold.png'
-  },
+  },  
   {
     title: "Internet Banking / Mobile Banking",
-    description: `I am the lead developer for the new version of the IB/MB application for Android, iOS, and web (one code base), which will replace the old application. This application is a full-fledged mobile/internet banking system which has all the latest functionalities related to the banking world.`,
+    description: `As the lead developer, I spearheaded the development of the new Internet Banking (IB) and Mobile Banking (MB) application for Android, iOS, and web, all from a single code base. This comprehensive, full-fledged banking system replaces the previous version, introducing cutting-edge functionalities that cater to modern banking needs. The application supports secure money transfers, bill payments, airtime purchases, ticket and fee payments, and robust account management features. Additionally, it offers advanced functionalities such as real-time transaction notifications, detailed account statements, loan applications, investment tracking, and a highly secure authentication process, ensuring a seamless and efficient user experience.`,
     image: '/imgs/internetbankingloggedin.png'
   },
   {
